c9fd296dcf83b6e438310c49bb35508e594d36ae
Shankargoud <shankargouda057@gmail.com>
2020-10-05 18:36:39 +0530

    Terraform code to provison VPC,IGW, Subnet on AWS

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "region-worker" {
+  type    = string
+  default = "us-west-2"
+}
+
